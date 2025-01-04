2a7c2052101dacea2a00a8dea2603af9bfcedbee
BettyNyambura <btynyambura20@gmail.com>
2025-01-04 09:01:07 -0800

    mm

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,12 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+         if cls:
+            return {
+                key: obj
+                for key, obj in FileStorage.__objects.items()
+                if isinstance(obj, cls)
+            }
         return FileStorage.__objects
 
     def new(self, obj):
