f7793a18dd62f03bb5391f67e0c6f3a2462b99e2
BettyNyambura <btynyambura20@gmail.com>
2025-01-07 13:14:59 -0800

    mm

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
                 self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
+            if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
