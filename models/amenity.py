884a9fae5263f1d4cd19ec8041fc3ec4dafed4e9
BettyNyambura <btynyambura20@gmail.com>
2025-01-07 14:28:39 -0800

    mm

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Amenity module for the HBNB project """
+""" Amenity module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -10,6 +11,7 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
 
+    # Many-to-Many relationship with Place using the place_amenity table
     place_amenities = relationship(
-        "Place", secondary="place_amenity", back_populates="amenities"
+        "Place", secondary="place_amenity", backref="amenities"
     )
--- models/place.py
+++ models/place.py
@@ -32,6 +32,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
     reviews = relationship(
         "Review", backref="place", cascade="all, delete, delete-orphan"
