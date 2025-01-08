cfdd0b6e3b279d37c06c6c970d59c4a605b66eeb
BettyNyambura <btynyambura20@gmail.com>
2025-01-08 01:03:46 -0800

    mm

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,6 +63,6 @@ class FileStorage:
     def delete(self, obj=None):
         """delete an object"""
         if obj is not None:
-            key = obj.__class__.__name__+'.'+obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            FileStorage.__objects.pop(key, None)
+
