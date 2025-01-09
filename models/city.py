2d6bbe0ddaf419a2702216d206e9ed353b98a9cc
BettyNyambura <btynyambura20@gmail.com>
2025-01-09 08:12:05 -0800

    mm

--- models/city.py
+++ models/city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 """ holds class City"""
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -9,8 +8,8 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
+    """Representation of city"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
