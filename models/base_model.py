3411042d131909a7382a688b9998b7bb9dc08eea
BettyNyambura <btynyambura20@gmail.com>
2025-01-09 08:29:06 -0800

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -13,18 +13,18 @@ import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+Base = declarative_base()
 
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+
+    id = Column(String(60), primary_key=True) if getenv(
+        "HBNB_TYPE_STORAGE") == "db" else None
+    created_at = Column(DateTime, default=datetime.utcnow) if getenv(
+        "HBNB_TYPE_STORAGE") == "db" else None
+    updated_at = Column(DateTime, default=datetime.utcnow) if getenv(
+        "HBNB_TYPE_STORAGE") == "db" else None
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
